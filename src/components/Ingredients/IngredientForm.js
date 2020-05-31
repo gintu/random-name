d5fa2f5894b80692511b1b0a433af2f2901cfac8
gintu tom <gintutom@gmail.com>
2020-05-31 23:42:31 +0530

    hooks rerendering issue

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
+import produce from "immer";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
 const IngredientForm = React.memo(props => {
+  const [state, setState] = useState({ title: "", amount: "" });
   const submitHandler = event => {
+    console.log(state);
     event.preventDefault();
+    props.addUserIngredients(state);
+
     // ...
   };
 
@@ -15,11 +20,33 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={state.title}
+              onChange={() =>
+                setState(
+                  produce(draft => {
+                    draft.title = event.target.value;
+                  })
+                )
+              }
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={state.amount}
+              onChange={() =>
+                setState(
+                  produce(draft => {
+                    draft.amount = event.target.value;
+                  })
+                )
+              }
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
 const Search = React.memo(props => {
+  let { searchFilter } = props;
+  let [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    let query =
+      searchInput.length === 0
+        ? ""
+        : `?orderBy="title"&equalTo="${searchInput}"`;
+
+    fetch(`https://hooks-primer.firebaseio.com/ingredients.json` + query)
+      .then(res => res.json())
+      .then(resData => {
+        let responseArray = [];
+        console.log("hi");
+
+        for (let item in resData) {
+          responseArray.push({
+            id: item,
+            title: resData[item].title,
+            amount: resData[item].amount
+          });
+        }
+        // searchFilter(responseArray);
+      });
+  }, [searchInput, searchFilter]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={searchInput}
+            onChange={e => setSearchInput(e.target.value)}
+          />
         </div>
       </Card>
     </section>
